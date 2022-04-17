ab5577884c1c4fd436f99c13eafcaa770a896f1c
Ben Funk <benfunk24@gmailcom>
2022-04-17 17:16:16 -0500

    rinkeby test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0x604DEcaff41AD38Fa411E7de132209aE7a62aaAE"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
